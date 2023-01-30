c188a87b5b76777be09b0c573e9e814283c4432e
Nutthakit Anansuksakul <ing118199@gmail.com>
2023-01-30 09:05:08 +0700

    done

---
+++ pages/categories/index.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+useHead({
+ title: "Categories",
+ meta: [
+   {
+     name: "description",
+     content: "Categories",
+   },
+ ],
+});
+</script>
+
+
+
+<template>
+ <section class="py-10">
+   <div class="container mx-auto">
+     <div class="flex flex-wrap gap-5">
+
+
+
+       <NuxtLink
+         to="/categories/clean-code"
+         class="flex items-center justify-center py-2 px-4 rounded text-white shadow-md hover:shadow-lg duration-200 text-2xl uppercase bg-blue-600">
+         <span class="font-semibold">#CLEAN CODE</span>
+       </NuxtLink>
+
+
+
+       <NuxtLink
+         to="/categories/clean-code"
+         class="flex items-center justify-center py-2 px-4 rounded text-white shadow-md hover:shadow-lg duration-200 text-2xl uppercase bg-blue-600">
+         <span class="font-semibold">#JAVASCRIPT</span>
+       </NuxtLink>
+
+
+
+       <NuxtLink
+         to="/categories/clean-code"
+         class="flex items-center justify-center py-2 px-4 rounded text-white shadow-md hover:shadow-lg duration-200 text-2xl uppercase bg-blue-600">
+         <span class="font-semibold">#DEVOPS</span>
+       </NuxtLink>
+
+
+
+     </div>
+   </div>
+ </section>
+</template>
+
---
+++ pages/contact.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup></script>
+
+
+
+<template>
+ <section class="bg-gray-100">
+   <div class="container mx-auto py-12 flex items-center flex-wrap">
+     <h1 class="text-2xl font-bold">Contact us</h1>
+     <p class="py-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit atque eos reiciendis! Deserunt facere nemo doloribus quae tempore. Itaque non possimus soluta delectus alias consectetur adipisci libero minima nam explicabo!</p>
+   </div>
+ </section>
+</template>
+
+
+
+<style scoped>
+
+
+
+</style>
+
